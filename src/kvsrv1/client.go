d53bd6584e897a694d113e82dee3819161458016
ylhuang <ylhuang0913@163.com>
2025-12-22 15:34:36 +0800

    my work on lab2 is done

--- src/kvsrv1/client.go
+++ src/kvsrv1/client.go
@@ -4,6 +4,7 @@ import (
 	"6.5840/kvsrv1/rpc"
 	"6.5840/kvtest1"
 	"6.5840/tester1"
+	"time"
 )
 
 
@@ -31,14 +32,15 @@ func MakeClerk(clnt *tester.Clnt, server string) kvtest.IKVClerk {
 func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 	// You will have to modify this function.
 	args := rpc.GetArgs{Key: key}
-	reply := rpc.GetReply{}
 
-	ok := ck.clnt.Call(ck.server, "KVServer.Get", &args, &reply)
-	if !ok {
-		return "", 0, rpc.ErrNoKey
+	for {
+		reply := rpc.GetReply{}
+		ok := ck.clnt.Call(ck.server, "KVServer.Get", &args, &reply)
+		if ok {
+			return reply.Value, reply.Version, reply.Err
+		}
+		time.Sleep(100*time.Millisecond)
 	}
-	
-	return reply.Value, reply.Version, reply.Err
 }
 
 // Put updates key with value only if the version in the
@@ -65,11 +67,32 @@ func (ck *Clerk) Put(key, value string, version rpc.Tversion) rpc.Err {
 		Value: 	 value,
 		Version: version,
 	}
-	reply := rpc.PutReply{}
 
-	ok := ck.clnt.Call(ck.server, "KVServer.Put", &args, &reply)
-	if !ok {
-		return "Timeout"
+	isRetry := false
+
+	for {
+		reply := rpc.PutReply{}
+		ok := ck.clnt.Call(ck.server, "KVServer.Put", &args, &reply)
+		if ok {
+			// success or Key doesnot exist
+			if reply.Err == rpc.OK || reply.Err == rpc.ErrNoKey {
+				return reply.Err
+			}
+
+			if reply.Err == rpc.ErrVersion {
+				if isRetry {
+					// 重试时遇到的版本错误，
+					// 有可能是上一次请求成功但回复丢了
+					return rpc.ErrMaybe
+				} else {
+					// 第一次发就遇到版本错误
+					// 别人抢先了，明确的失败
+					return rpc.ErrVersion
+				}
+			}
+			return reply.Err
+		}
+		// ok == false --> retry
+		isRetry = true
 	}
-	return reply.Err
 }
--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -2,6 +2,8 @@ package lock
 
 import (
 	"6.5840/kvtest1"
+	"6.5840/kvsrv1/rpc"
+	"time"
 )
 
 type Lock struct {
@@ -9,8 +11,10 @@ type Lock struct {
 	// the specific Clerk type of ck but promises that ck supports
 	// Put and Get.  The tester passes the clerk in when calling
 	// MakeLock().
-	ck kvtest.IKVClerk
+	ck   kvtest.IKVClerk
 	// You may add code here
+	name string
+	id   string 
 }
 
 // The tester calls MakeLock() and passes in a k/v clerk; your code can
@@ -19,15 +23,66 @@ type Lock struct {
 // Use l as the key to store the "lock state" (you would have to decide
 // precisely what the lock state is).
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
-	lk := &Lock{ck: ck}
+	lk := &Lock{
+		ck:   ck,
+		name: l,
+		id: kvtest.RandValue(8),
+	}
 	// You may add code here
 	return lk
 }
 
 func (lk *Lock) Acquire() {
 	// Your code here
+	myToken := lk.id
+	for {
+		// get lock's state
+		val, ver, err := lk.ck.Get(lk.name)
+
+		// check lock is occupied or not
+		if err == rpc.ErrNoKey || (err == rpc.OK && val == "") {
+			putVer := ver
+			if err == rpc.ErrNoKey {
+				putVer = 0
+			}
+
+			err2 := lk.ck.Put(lk.name, myToken, putVer) // locked
+			if err2 == rpc.OK {
+				return
+			}
+			if err2 == rpc.ErrMaybe {
+				checkVal, _, checkErr := lk.ck.Get(lk.name)
+				if checkErr == rpc.OK && checkVal == myToken {
+					return
+				}
+			}
+		}
+		time.Sleep(10*time.Millisecond)
+	}
 }
 
 func (lk *Lock) Release() {
 	// Your code here
+	for {
+		val, ver, err := lk.ck.Get(lk.name)
+
+		if err == rpc.ErrNoKey || (err == rpc.OK && val == "") {
+			return
+		}
+		if err == rpc.OK && val != lk.id {
+			return
+		}
+
+		err2 := lk.ck.Put(lk.name, "", ver)
+		if err2 == rpc.OK {
+			return
+		}
+		if err2 == rpc.ErrMaybe {
+			checkVal, _, checkErr := lk.ck.Get(lk.name)
+			if checkErr == rpc.ErrNoKey || (checkErr == rpc.OK && checkVal != lk.id) {
+				return
+			}
+		}
+		time.Sleep(10*time.Millisecond)
+	}
 }
